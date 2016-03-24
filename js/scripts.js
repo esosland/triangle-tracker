f1d16f8b93febb791a6490f9c3f30f7db2a0a3ed
Elissa Sosland and Garrett Biernat <garrett@>
2016-03-24 16:04:35 -0700

    add pictures

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,37 @@ $(function(){
 			if(side1 === side2 && side2 === side3){
 				$("#answer").text("An Equilateral Triangle");
 				$("#hanswer").show();
+				$("#eq").show();
+				$("#iso").hide();
+				$("#sca").hide();
+				$("#yoda").hide();
+
 
 			}
 			if((side1 === side2 && side2 != side3) || (side1 === side3 && side1 != side2) || (side2 === side3 && side1 != side3)){
 				$("#answer").text("An Isosceles Triangle");
 				$("#hanswer").show();
+				$("#eq").hide();
+				$("#iso").show();
+				$("#sca").hide();
+				$("#yoda").hide();
 
 			}
 			if(side1 != side2 && side1 != side3 && side2 != side3){
 				$("#answer").text("An Scalene Triangle");
 				$("#hanswer").show();
+				$("#eq").hide();
+				$("#iso").hide();
+				$("#sca").show();
+				$("#yoda").hide();
 			}
 		}else{
 			$("#answer").text("is not a triangle");
 			$("#hanswer").show();
+			$("#eq").hide();
+			$("#iso").hide();
+			$("#sca").hide();
+			$("#yoda").show();
 		}
 		event.preventDefault();
 	});
